c10cbed6c01b9b8fd8543d2b4240240405c92a2e
Ipsha Menon <ipsha.menon@gmail.com>
2022-08-15 12:04:59 -0700

    added ejs

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const { response } = require("express");
 const express = require ("express");
-const app = express();
+const ej = require("ejs");
 const https = require('https');
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+const app = express();
 
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const resultsInfo = [];
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
+    res.render("index", {results: resultsInfo});
+   
 });
+
+app.get("/results", function(req, res){
+    res.render("results", {results: resultsInfo});
+   
+});
+
 
 app.post("/", function (req, res){
     const query = req.body.cityName;
     const apiKey = "0268716c6b42ef1495b318573786b781";
     const units = "imperial"
     
-    const url =  "https://api.openweathermap.org/data/2.5/weather?q="+ query + "&units=" + units + "&appid=" + apiKey;
+    const url =  "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + units + "&appid=" + apiKey;
     
     https.get(url, function(response){
         console.log(response.statusCode);
@@ -26,14 +40,20 @@ app.post("/", function (req, res){
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
             const imageURL = "http://openweathermap.org/img/wn/" + icon +"@2x.png";
-
-            res.write("<h1>The temperature in " + query +  " is " + temperature + " degrees Fahrenheit</h1>");
-            res.write("<h3> The weather is currently " + weatherDescription + "</h3>");
-            res.write("<img src=" + imageURL + ">");
-            res.send();
+            const result = {
+                query: query,
+                temperature: temperature,
+                weatherDescription: weatherDescription,
+                icon: icon,
+            }
+            
+            resultsInfo.unshift(result);
+            res.redirect("results");
 
         });
+        
     });
+   
 })
 
 app.listen(3000, function(){
