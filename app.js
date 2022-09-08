a192de5b06d8ae987e34b7ff6e245a5a629e5586
Ipsha Menon <ipsha.menon@gmail.com>
2022-09-08 13:58:55 -0700

    fixed results card styles

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.post("/", function (req, res){
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
             const countryCode = weatherData.sys.country;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon +"@2x.png";
+            const imageURL = "http://openweathermap.org/img/wn/" + icon +".png";
             const result = {
                 query: query,
                 temperature: Math.round(temperature),
